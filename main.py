a56ecb525c41d6be4600fc7bdfdb5b80ae9f9fd9
stuartelimu <stuartelimu@gmail.com>
2021-05-17 10:36:06 +0300

    create get pizzas endpoint

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ from fastapi import FastAPI
 # initialize app
 app = FastAPI()
 
+# pizzas database
+db = [
+    {
+        'id': 0,
+        'name': 'Neapolitan Pizza',
+        'toppings': ['Basil', 'Onions'],
+    },
+    {
+        'id': 1,
+        'name': 'Chicago Pizza',
+        'toppings': ['Cheese', 'Tomatoes', 'Onions'],
+    },
+]
+
 @app.get('/')
 async def index():
     return {"greeting": "Hello, world!"}
+
+# get all pizzas
+@app.get('/pizzas')
+async def pizzas():
+    return db
