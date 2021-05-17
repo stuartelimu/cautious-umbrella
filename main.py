dc1beb94b412c9352f47042a145bff1953638698
stuartelimu <stuartelimu@gmail.com>
2021-05-17 11:05:42 +0300

    create post end point

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 # initialize app
 app = FastAPI()
@@ -17,6 +19,11 @@ db = [
     },
 ]
 
+# pizza model
+class Pizza(BaseModel):
+    name: str
+    toppings: List[str]
+
 @app.get('/')
 async def index():
     return {"greeting": "Hello, world!"}
@@ -33,3 +40,15 @@ async def get_pizzas(pizza_id):
         if(pizza['id'] == int(pizza_id)):
             return pizza
     return {"error": "Pizza not found with id: " + pizza_id }
+
+# create a new pizza
+@app.post('/pizzas/create')
+async def create_pizza(pizza: Pizza):
+    # convert to dictionary
+    new_pizza = pizza.dict()
+    # add new id
+    new_pizza['id'] = db[-1]['id'] + 1
+    # add to list
+    print(new_pizza)
+    db.append(new_pizza)
+    return new_pizza
